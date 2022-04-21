baa8ed6ebe742a35a544a0984681f67606c0d6da
mhutchinson11 <98057736+mhutchinson11@users.noreply.github.com>
2022-04-21 15:05:24 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+
+        String[] names = {"Mikayla", "Jemichael", "Alton"};
+        int[] grades = {99, 188, 72};
+        for (int i = 0; i < names.length; i++)
+        {
+            System.out.println(names[0] + " got a grade of " + grades[0]);
+        }
+    }
+}
